b59a9bb3eb362140252f06c08865bd17a26ecdde
Danish <danishabdullah276@gmail.com>
2024-06-10 15:20:39 +0530

    ik

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,95 @@
+import { useContext } from "react";
+import { assets } from "../../assets/assets";
+import "./main.css";
+import { Context } from "../../context/Context";
+const Main = () => {
+	const {
+		onSent,
+		recentPrompt,
+		showResults,
+		loading,
+		resultData,
+		setInput,
+		input,
+	} = useContext(Context);
+
+    const handleCardClick = (promptText) => {
+			setInput(promptText);
+		};
+	return (
+		<div className="main">
+			<div className="nav">
+				<p>parakram</p>
+				<img src={assets.user} alt="" />
+			</div>
+			<div className="main-container">
+				{!showResults ? (
+					<>
+						<div className="greet">
+							<p>
+								<span>Jai Hind </span>
+							</p>
+							<p>Delhi police Apki sahayta mein</p>
+						</div>
+						<div className="cards">
+							<div className="dpji">
+								<img src="src\assets\policed.png" alt="" />
+							</div>
+							
+						</div>
+					</>
+				) : (
+					<div className="result">
+						<div className="result-title">
+							<img src={assets.user} alt="" />
+							<p>{recentPrompt}</p>
+						</div>
+						<div className="result-data">
+							<img src={assets.gemini_icon} alt="" />
+							{loading ? (
+								<div className="loader">
+									<hr />
+									<hr />
+									<hr />
+								</div>
+							) : (
+								<p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+							)}
+						</div>
+					</div>
+				)}
+
+				<div className="main-bottom">
+					<div className="search-box">
+						<input
+							onChange={(e) => {
+								setInput(e.target.value);
+							}}
+							value={input}
+							type="text"
+							placeholder="suchna yehan daalein"
+						/>
+						<div>
+							<img src={assets.gallery_icon} alt="" />
+							<img src={assets.mic_icon} alt="" />
+							<img
+								src={assets.send_icon}
+								alt=""
+								onClick={() => {
+									onSent();
+								}}
+							/>
+						</div>
+					</div>
+					<div className="bottom-info">
+						<p>
+						Delhi Police - Shanti Seva Nyaya (शांति सेवा न्याय)
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Main;
